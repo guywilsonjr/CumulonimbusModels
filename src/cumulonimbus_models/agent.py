13231334aed5c57ef469c3925c7710e918766ebf
Guy Wilson <Guywilsonjr@gmail.com>
2024-05-06 12:44:57 -0700

    Remove ifcs from agent registration request

--- src/cumulonimbus_models/agent.py
+++ src/cumulonimbus_models/agent.py
@@ -1,13 +1,9 @@
-from typing import Optional
-
 from pydantic import BaseModel
-
-from cumulonimbus_models.network import NetworkInterface
 
 
 class AgentRegisterRequest(BaseModel):
     hostname: str
-    ifcs: list[NetworkInterface]
+
 
 
 
