60e353139760c5d58f32d7a52c211116ecf985ff
Guy Wilson <Guywilsonjr@gmail.com>
2024-05-12 18:24:13 -0700

    Update URL func

--- src/cumulonimbus_models/api.py
+++ src/cumulonimbus_models/api.py
@@ -6,3 +6,7 @@ from pydantic import BaseModel, Field
 class APIRequest(BaseModel):
     path: ClassVar[str] = Field(exclude=True)
     format: ClassVar[str] = Field(exclude=True, const=True)
+
+    def get_url(self, base_url: str) -> str:
+        return f'{base_url}/{self.path}'
+
